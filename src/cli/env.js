7dc155c2d9a03e54ea431eef18d62547a9a21ed3
yurilabatsevich <yurilabatsevich@gmail.com>
2023-06-13 22:40:04 +0300

    feat: env.js done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const result =  Object.entries(process.env).filter((value) => { 
+    const [key,val] = value; 
+     if(key.startsWith('RSS')) return value;
+}).map((entry) => entry.join('=')).join('; ')
+
+  process.stdout.write(result)
+
 };
 
 parseEnv();
