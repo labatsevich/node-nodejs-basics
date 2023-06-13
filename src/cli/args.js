a84ea4ca8ddc19e99fd17aa17e0364517913eb92
yurilabatsevich <yurilabatsevich@gmail.com>
2023-06-13 23:22:10 +0300

    feat: args.js done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,4 +1,13 @@
 const parseArgs = () => {
+ const arr =  process.argv.slice(2);
+ const temp = []
+
+ while(arr.length){
+  temp.push(arr.splice(0,2).join(' is ').replace('--',''))
+ }
+
+ process.stdout.write(temp.join(', '))
+
 };
 
 parseArgs();
