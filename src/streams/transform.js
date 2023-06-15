31c09d76bbcd2044ab8770d6e98c33b67ed8057f
yurilabatsevich <yuralabatsevich@gmail.com>
2023-06-15 14:39:04 +0300

    feat: transform.js task completed

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import {stdin,stdout} from 'node:process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    
+ const reverse = new Transform({
+    transform(chunk,encoding,callback){
+        callback(null,[...chunk.toString()].reverse().join(''))
+    }
+ })
+    
+ stdin.pipe(reverse).pipe(stdout)
+
 };
 
 await transform();
