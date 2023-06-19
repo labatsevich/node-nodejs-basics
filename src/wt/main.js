049c41c4d213566becb046873edcfc50c7d042d4
yurilabatsevich <yuralabatsevich@gmail.com>
2023-06-19 12:56:30 +0300

    feat: wt exercises completed

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { cpus } from 'node:os';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Worker } from 'node:worker_threads';
+
+const __filename = fileURLToPath(import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+
+
+
+    const arr = cpus().map((item, i) => new Promise((res, rej) => {
+
+        const worker = new Worker(path.join(dirname(__filename), 'worker.js'), {
+            workerData: 10 + i
+        });
+
+        worker.on('message', (message) => res(message));
+        worker.on('error', (error) => rej(error));
+
+    }));
+
+    const result = await Promise.allSettled(arr);
+    const res = result.map((item) => {
+        return {
+            status: item.status === 'fulfilled' ? 'resolved' : 'error',
+            data: item.status === 'fulfilled' ? item.value : null
+        }
+    });
+
+    console.log(res);
+
 };
 
 await performCalculations();
